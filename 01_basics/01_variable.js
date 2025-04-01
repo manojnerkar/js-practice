0d1835a7b8419149309fb5e8ecce28227cb12710
MANOJ NERKAR <79189614+manojnerkar@users.noreply.github.com>
2025-04-01 08:51:28 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 12345;
+let accountEmail = "manojnerkar90@gmail.com";
+var accoutPassword = "12345";
+accountCity = "Jalgaon";
+
+
+
+accountEmail = "hr@mnoj.com";
+accoutPassword = "12345";
+accountCity = "Pimpalner";
+
+console.table([accountId,accountEmail,accoutPassword,accountCity]) 
+
+
+/*
+Prefer not to use var
+bacouse of issue in block scope and funtianl scope
+*/
