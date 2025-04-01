dcf5d494cb5b7bcc2b81af9dc95cd1607d8f044d
MANOJ NERKAR <79189614+manojnerkar@users.noreply.github.com>
2025-04-01 09:10:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treact all js code as newer version
+
+// alear(3+3)  we are using node.js not browser
+
+
+// console.log(3
+    
+    +3); // code readabilty  should be high
+// console.log("Hello");
+
+
+
+let namle = "Manoj"
+let age = 28;
+let isLoggedIn = false;
+
+
+/*
+    numer => 2 to power 53
+    bigint
+    string => 
+    boolean => true/false
+    null => standalone value
+    undefined =>
+    symbol => unique
+
+    //object
+*/
+
+
+console.log(typeof null);
+
+
