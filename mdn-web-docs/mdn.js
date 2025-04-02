586272f367e4e871ba4264825520c6c54678c21f
MANOJ NERKAR <79189614+manojnerkar@users.noreply.github.com>
2025-04-02 17:22:22 +0000

    mdn

---
+++ mdn-web-docs/mdn.js
@@ -0,0 +1,23 @@
+// const foo = () => {
+//     console.log("Hello");
+// }
+
+
+// foo();
+
+
+//We assigned an Anonymous Function in a Variable, then we used that variable to invoke the function by adding parentheses () at the end.?
+
+
+
+function hello1(){
+    return "Hello";
+}
+
+
+function hello2(a , b){
+    console.log(a() + b);
+}
+
+
+hello2(hello1 , "manoj")
